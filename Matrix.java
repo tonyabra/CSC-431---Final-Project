7449fdfec28832788fcca205d7d86e484ec5cafb
tonyabra <tonyabra@gmail.com>
2012-02-12 22:41:24 -0600

    Matrix upload

--- Matrix.java
+++ Matrix.java
@@ -6,7 +6,7 @@ class Matrix {
     
     private double[][] data;
 
-    
+    //Constructor to fill matrix with value
     public Matrix(int rowsIn, int colsIn, double fill){
 	//Iterator variables
 	int cCount, rCount;
@@ -23,16 +23,32 @@ class Matrix {
 	}		
     }
 
+    //Constructor to fill matrix with value
+    public Matrix(int rowsIn, int colsIn, double fill){
+	//Iterator variables
+	int cCount, rCount;
+
+	rows = rowsIn;
+	cols = colsIn; 
+	
+	data = new float[rows][cols];
+	
+    }
+
     //Function to return an item
-    public float getItem (int row, int col){   
+    public double getItem (int row, int col){   
 	return data[row][col];
     }
 
     //Function to set an item
-    public float setItem (int row, int col, float value){
+    public double setItem (int row, int col, float value){
 	data[row][col] = value;
 	return value;
     }
+
+    //Function to return a row
+    public double
+    
 
     //A function to print the matrix
     public void printMatrix(){
@@ -52,9 +68,7 @@ class Matrix {
 	    System.out.println(currLine);
 	}
 
-    }
-
-    
+    }    
 
     public static void main(String[] args){
 
