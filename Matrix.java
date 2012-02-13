fa5666d4a8afa3bbdcc6f540258f6b0837873628
tonyabra <tonyabra@gmail.com>
2012-02-13 01:17:23 -0600

    Implemented the functions up through add

--- Matrix.java
+++ Matrix.java
@@ -6,33 +6,49 @@ class Matrix {
     
     private double[][] data;
 
-    //Constructor to fill matrix with value
-    public Matrix(int rowsIn, int colsIn, double fill){
+    //Constructor to create the empty matrix
+    public Matrix(int rowsIn, int colsIn){
 	//Iterator variables
 	int cCount, rCount;
 
 	rows = rowsIn;
 	cols = colsIn; 
-	
-	data = new float[rows][cols];
 
-	for (rCount = 0; rCount < rows; ++rCount){
-	   for (cCount = 0; cCount < cols; ++cCount){
-	       data[rCount][cCount] = fill;
-	    }
-	}		
+	data = new double[rows][cols];	
+
     }
 
-    //Constructor to fill matrix with value
-    public Matrix(int rowsIn, int colsIn, double fill){
+    //Constructor to fill matrix with a double array
+    public Matrix(int rowsIn, int colsIn, double[][] fill){
 	//Iterator variables
 	int cCount, rCount;
 
 	rows = rowsIn;
 	cols = colsIn; 
-	
-	data = new float[rows][cols];
-	
+
+	data = fill.clone();
+
+    }
+
+    //Constructor to add a single array
+    public Matrix(int rowsIn, int colsIn, double[] fill){
+	//Iterator variables
+	int cCount, rCount;
+
+	rows = rowsIn;
+	cols = colsIn; 
+
+	data = new double[rows][cols];
+
+	//If there is only one column, fill the column
+	if (cols == 1){
+	    for (rCount = 0; rCount < rows; ++rCount){
+		data[rCount][0] = fill[rCount];
+	    }  
+	}
+	else{ //Else, fill the first row
+	    data[0] = fill;	    
+	}
     }
 
     //Function to return an item
@@ -41,14 +57,112 @@ class Matrix {
     }
 
     //Function to set an item
-    public double setItem (int row, int col, float value){
+    public double setItem (int row, int col, double value){
 	data[row][col] = value;
 	return value;
     }
 
     //Function to return a row
-    public double
-    
+    public Matrix row (int rowIn){
+
+	double[] rowVals = new double[cols];
+	
+	System.arraycopy(data[rowIn], 0, rowVals, 0, data[rowIn].length);
+
+	return new Matrix(1, cols, rowVals); 
+    }
+
+    //Function to return a column
+    public Matrix col (int colIn){
+
+	int rCount;
+	
+	double[] colVals = new double[rows];
+	
+	for (rCount = 0; rCount < rows; ++rCount){
+	    colVals[rCount] = data[rCount][colIn];
+	}
+	
+	return new Matrix(rows, 1, colVals); 
+    }	
+
+    //Function to return the matrix as a list
+    //One row at a time
+    public double[] as_list (){
+	int rCount, cCount, counter = 0;
+	double[] retVal = new double[cols*rows];
+	
+	for (rCount = 0; rCount < rows; rCount++){
+	    for (cCount = 0; cCount < cols; cCount++){
+		retVal[counter] = data[rCount][cCount];
+		counter++;
+	    }
+	} 
+
+	return retVal;
+    }
+
+    //Function to return the array as a list
+    public String toString(){
+	double[] retVal = this.as_list();
+
+	return java.util.Arrays.toString(retVal);
+    }
+
+    //Function to create the identity matrix
+    public Matrix identity(int rows){
+	int rCount;
+	Matrix retMatrix = new Matrix(rows, rows);
+
+	for (rCount = 0; rCount < rows; ++rCount){
+	    retMatrix.setItem(rCount, rCount, 1);
+	}
+
+	return retMatrix;
+    }
+
+    //Lays the values along the diagonal
+    public Matrix diagonal(double[] vals){
+	int rCount;
+	Matrix retMatrix = new Matrix(vals.length, vals.length);
+
+	for (rCount = 0; rCount < vals.length; ++rCount){
+	    retMatrix.setItem(rCount, rCount, vals[rCount]);
+	}
+
+	return retMatrix;
+    }
+
+    //Adds to matrices together provided they have the same dimensions
+    public Matrix add (Matrix matA, Matrix matB){
+	
+	int rCount, cCount;
+	Matrix matC = null;
+
+	if (checkDimensions(matA, matB)){
+	    
+	    matC = new Matrix(matA.rows, matB.rows);
+
+	    for (rCount = 0; rCount < matC.rows; rCount++){
+		for (cCount = 0; cCount < matC.cols; cCount++){
+		    matC.setItem(rCount, cCount, matA.getItem(rCount, cCount) + matB.getItem(rCount, cCount));
+		}
+	    }	   
+
+	}
+
+	return matC;
+    }
+
+    //Helper function to ensure the dimensions are the same
+    public boolean checkDimensions(Matrix matA, Matrix matB){
+	if ((matA.cols == matB.cols) && (matA.rows == matB.rows)){
+	   return true;
+	 }
+	else{
+	    return false;
+	} 
+    }
 
     //A function to print the matrix
     public void printMatrix(){
@@ -72,8 +186,27 @@ class Matrix {
 
     public static void main(String[] args){
 
-	Matrix testMatrix = new Matrix(2, 2, 1);
+	//Using this main class for testing
+	Matrix testMatrix = new Matrix(6, 6);
+	double testVal = 45.4543243;
 
-	testMatrix.printMatrix();
+	testMatrix.setItem(1, 1, testVal);
+	testMatrix.setItem(1, 2, testVal);
+
+	Matrix newMatrix = testMatrix.col(1);
+
+	double[] vals = new double[4];
+	vals[0] = 0;
+	vals[1] = 1;
+	vals[2] = 2;
+	vals[3] = 3;
+
+	testMatrix = newMatrix.diagonal(vals);
+	newMatrix = newMatrix.identity(4);
+	
+	Matrix threeMatrix = testMatrix.add(testMatrix, newMatrix);
+
+	threeMatrix.printMatrix();
+	
     }
 }
